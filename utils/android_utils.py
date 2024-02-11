570e628d15be653e19d00cfa6682bbf2d912a441
Denys Reryt <denysreryt28@gmail.com>
2024-02-11 14:47:37 +0200

    dynamic definition of udid

--- utils/android_utils.py
+++ utils/android_utils.py
@@ -1,3 +1,23 @@
+import subprocess
+
+def get_udid():
+    '''
+    By default using first device from list.
+    '''
+    try:
+        result = subprocess.run(['adb', 'devices'], capture_output=True, text=True, check=True)
+        devices = result.stdout.strip().split('\n')[1]
+        if devices:
+            udid = devices.split('\t')[0]
+            return udid
+        else:
+            print("No devices found.")
+            return None
+    except subprocess.CalledProcessError as ex:
+        print(f"Error executing 'adb devices': {ex}")
+        return None
+    
+
 def android_get_desired_capabilities():
     return {
         'platformName': 'Android',
@@ -5,7 +25,7 @@ def android_get_desired_capabilities():
         'deviceName': 'Pixel 7 Pro API 33',
         'platformVersion': '13',
         'newCommandTimeout': 500,
-        'udid': 'emulator-5554',
+        'udid': get_udid(),
         'appActivity': 'com.ajaxsystems.ui.activity.LauncherActivity',
         'appPackage': 'com.ajaxsystems',
         'systemPort': 8301,
